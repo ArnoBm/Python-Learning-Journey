b206396d09dea02b0d86f87664df3bea3cff09db
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-05 13:37:03 +0600

    Add files via upload

---
+++ Sequence Operators.py
@@ -0,0 +1,17 @@
+string1 = "Hello "
+string2 = "World!"
+string3 = "Python"
+string4 = "Programming"
+string5 = "is"
+string6 = "fun"
+
+print (string1 + string2 + " " + string3 + " " + string4 + " " + string5 + " " + string6)  # Output: HelloWorld Python Programming is fun
+
+print (string1 * 3)  # Output: Hello Hello Hello
+print (string1 * (5+2))
+
+today = "Monday"
+print ("day" in today)  # Output: True
+print ("Mon" in today)  # Output: True
+print ("Thur" in today)  # Output: False
+print ("Sat" is today)  # Output: False
