e7e374d7568acec89c1b87a348a77d7659c05ed0
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-09 12:06:05 +0600

    Add files via upload
    Understanding code execution: Code inside a loop/block runs repeatedly until the block completes, while code outside executes only after the block finishes.

---
+++ Blocks.py
@@ -0,0 +1,7 @@
+for i in range(1, 4):
+    print ("No. {} squared is {} and cubed is {}".format(i, i**2, i**3))
+print ("*" * 40)
+
+for i in range(1, 4):
+    print ("No. {} squared is {} and cubed is {}".format(i, i**2, i**3))
+    print ("*" * 10)
