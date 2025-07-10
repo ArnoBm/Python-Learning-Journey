c087b1eaf4028e7b0abce190ed7dfb2eb3d90608
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-10 18:48:20 +0600

    Add files via upload

---
+++ Notin.py
@@ -0,0 +1,5 @@
+activity = input("Raya: What would you like to do today?\nArnob: ")
+if "argue" not in activity.lower():
+    print("Raya: But I want to argue with you.")
+else:
+    print("Raya: Okay, let's argue then.")
