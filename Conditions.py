b5e7475b80d92f429c5b8f33aac44505c59dd40e
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-10 13:57:39 +0600

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age >= 20 and age <= 65:
+    print("You are eligible to work.")
+elif 10 <= age <= 19:
+    print("Learn for a better future.")
+else:
+    print("Enjoy your home life.")
