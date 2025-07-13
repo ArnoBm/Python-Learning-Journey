2d6b7c459d9b777ae556496d0beb4de0465f07e3
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-13 18:18:56 +0600

    Add files via upload
    Updated code. Break added.

--- Adventure.py
+++ Adventure.py
@@ -4,11 +4,17 @@ while searching_name not in name:
     searching_name = input("Enter a name to search: ")
     if searching_name == "Arnob":
         print (f"{searching_name} lives in Rajshahi")
+        break
     elif searching_name == "Raya":
         print (f"{searching_name} lives in Dinajpur")
+        break
     elif searching_name == "Tabsheer":
         print (f"{searching_name} lives in Dhaka")
+        break
     elif searching_name == "Tonima":
         print (f"{searching_name} lives in Chittagong")
+        break
     else:
         print(f"{searching_name} not found in the list")
+        #break
+print("Thank you for using the search program!")
