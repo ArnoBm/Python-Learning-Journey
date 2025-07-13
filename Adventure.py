7af7662f538e5918433c1a499c4f620a167ff294
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-13 18:26:34 +0600

    Add files via upload

--- Adventure.py
+++ Adventure.py
@@ -2,16 +2,16 @@ name = ["Arnob", "Raya", "Tabsheer", "Tonima"]
 searching_name = ""
 while searching_name not in name:
     searching_name = input("Enter a name to search: ")
-    if searching_name == "Arnob":
+    if searching_name.casefold() == "arnob":
         print (f"{searching_name} lives in Rajshahi")
         break
-    elif searching_name == "Raya":
+    elif searching_name.casefold() == "raya":
         print (f"{searching_name} lives in Dinajpur")
         break
-    elif searching_name == "Tabsheer":
+    elif searching_name.casefold() == "tabsheer":
         print (f"{searching_name} lives in Dhaka")
         break
-    elif searching_name == "Tonima":
+    elif searching_name.casefold() == "tonima":
         print (f"{searching_name} lives in Chittagong")
         break
     else:
