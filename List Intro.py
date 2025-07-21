73ec151a17614b7a57c712c879976cd3918233e5
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-21 11:42:51 +0600

    Add files via upload

---
+++ List Intro.py
@@ -0,0 +1,8 @@
+computer_parts = ["monitor", "keyboard", "mouse", "CPU", "printer"]
+for part in computer_parts:
+    print(part)
+
+print()
+print (computer_parts[2])
+print (computer_parts[0:4])
+print (computer_parts[-1])
