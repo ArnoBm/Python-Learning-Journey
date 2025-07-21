477670da0cbe6ee11fe2b170e57bc215eb94935e
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-21 17:37:53 +0600

    Add files via upload

--- Mutable.py
+++ Mutable.py
@@ -6,3 +6,11 @@ print (id(another_list))
 shopping_list += ["butter"]
 print (shopping_list)
 print (id(shopping_list))
+
+a = b = c = d = e = f = another_list
+print(a)
+
+print ("Adding cookies")
+b.append ("cookies")
+print (e)
+print (f)
