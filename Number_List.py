a82eaae1deafab620682196b5948d9e2ab2bf2de
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-22 18:44:24 +0600

    Add files via upload

---
+++ Number_List.py
@@ -0,0 +1,12 @@
+empty_list = []
+odd = [1, 3, 5, 7, 9]
+even = [2, 4, 6, 8, 10]
+
+numbers = odd + even
+print (numbers)
+
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+
+digits = sorted("432985617")
+print (digits)
