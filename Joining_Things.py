e43f3c360305fd4c467e73f7ec03f00df124f2cb
ArnoBm <117790355+ArnoBm@users.noreply.github.com>
2025-07-25 15:28:14 +0600

    Add files via upload

---
+++ Joining_Things.py
@@ -0,0 +1,13 @@
+TrainName = [
+    "Madhumati Express",
+    "Banalata Express",
+    "Silkcity Express",
+    "Padma Express",
+    "Banglabandha Express",
+]
+
+separator = " | "
+output = separator.join(TrainName)
+print(output)
+
+print(", ".join(TrainName))
